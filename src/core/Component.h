b1ca120858a243d5514d0187c6f7052a2bc4a025
kavos113 <kohei.hadano@gmail.com>
2024-11-05 20:00:52 +0900

    init

---
+++ src/core/Component.h
@@ -0,0 +1,14 @@
+#ifndef ELEMENT_CORE_COMPONENT_H
+#define ELEMENT_CORE_COMPONENT_H
+
+namespace element
+{
+    
+    class Component
+    {
+    
+    };
+    
+} // element
+
+#endif //ELEMENT_CORE_COMPONENT_H
---
+++ src/core/Window.h
@@ -0,0 +1,14 @@
+#ifndef ELEMENT_WINDOW_H
+#define ELEMENT_WINDOW_H
+
+namespace element
+{
+    
+    class Window
+    {
+    
+    };
+    
+} // element
+
+#endif //ELEMENT_WINDOW_H
