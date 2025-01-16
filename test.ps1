d0672a70e01e85b15a678c79b12d9c2db6acbf24
kavos113 <kohei.hadano@gmail.com>
2025-01-16 11:30:48 +0900

    init2

--- test.ps1
+++
@@ -1,7 +0,0 @@
-$bin_dir = '.\cmake-build-debug\bin'
-
-echo "Searching for executables in $bin_dir..."
-Get-ChildItem -Recurse -Filter '*.exe*' -Path $bin_dir | ForEach-Object {
-    Write-Host "Running $($_.FullName)..."
-    & $_.FullName
-}
