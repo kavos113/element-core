b0172eedd774360a98483aabcf5be409c7fc80ae
kavos113 <kohei.hadano@gmail.com>
2025-02-10 15:45:57 +0900

    fix test

--- test/element/geometry/RectangleTest.cpp
+++ test/element/geometry/RectangleTest.cpp
@@ -16,7 +16,12 @@ protected:
 
 TEST_F(RectangleTest, CreateRectangle)
 {
-    element::Rectangle rect{X, Y, WIDTH, HEIGHT};
+    constexpr element::Rectangle rect{
+        .x = X,
+        .y = Y,
+        .width = WIDTH,
+        .height = HEIGHT
+    };
 
     ASSERT_EQ(rect.x, X);
     ASSERT_EQ(rect.y, Y);
@@ -26,40 +31,55 @@ TEST_F(RectangleTest, CreateRectangle)
 
 TEST_F(RectangleTest, Equality)
 {
-    element::Rectangle rect1{X, Y, WIDTH, HEIGHT};
-    element::Rectangle rect2{X, Y, WIDTH, HEIGHT};
+    constexpr element::Rectangle rect1{
+        .x = X,
+        .y = Y,
+        .width = WIDTH,
+        .height = HEIGHT
+    };
+    constexpr element::Rectangle rect2{
+        .x = X,
+        .y = Y,
+        .width = WIDTH,
+        .height = HEIGHT
+    };
 
     ASSERT_EQ(rect1, rect2);
 }
 
 TEST_F(RectangleTest, GetPosition)
 {
-    element::Rectangle rect{X, Y, WIDTH, HEIGHT};
+    constexpr element::Rectangle rect{
+        .x = X,
+        .y = Y,
+        .width = WIDTH,
+        .height = HEIGHT
+    };
 
     ASSERT_EQ(rect.GetPosition(), element::Point(X, Y));
 }
 
 TEST_F(RectangleTest, GetSize)
 {
-    element::Rectangle rect{X, Y, WIDTH, HEIGHT};
+    constexpr element::Rectangle rect{X, Y, WIDTH, HEIGHT};
 
     ASSERT_EQ(rect.GetSize(), element::Size(WIDTH, HEIGHT));
 }
 
 TEST_F(RectangleTest, SetPosition)
 {
-    element::Rectangle rect{X, Y, WIDTH, HEIGHT};
+    element::Rectangle rect{.x = X, .y = Y, .width = WIDTH, .height = HEIGHT};
 
-    rect.SetPosition(element::Point{X + 1.0f, Y + 1.0f});
+    rect.SetPosition(element::Point{.x = X + 1.0f, .y = Y + 1.0f});
 
     ASSERT_EQ(rect.GetPosition(), element::Point(X + 1.0f, Y + 1.0f));
 }
 
 TEST_F(RectangleTest, SetSize)
 {
-    element::Rectangle rect{X, Y, WIDTH, HEIGHT};
+    element::Rectangle rect{.x = X, .y = Y, .width = WIDTH, .height = HEIGHT};
 
-    rect.SetSize(element::Size{WIDTH + 1.0f, HEIGHT + 1.0f});
+    rect.SetSize(element::Size{.width = WIDTH + 1.0f, .height = HEIGHT + 1.0f});
 
     ASSERT_EQ(rect.GetSize(), element::Size(WIDTH + 1.0f, HEIGHT + 1.0f));
 }
