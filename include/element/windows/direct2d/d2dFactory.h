2fdc60d1de0c368b01c2e6b50351a4d7fa6aea35
kavos113 <kohei.hadano@gmail.com>
2025-02-11 15:26:57 +0900

    d2d factory

---
+++ include/element/windows/direct2d/d2dFactory.h
@@ -0,0 +1,38 @@
+#ifndef D2DFACTORY_H
+#define D2DFACTORY_H
+
+#include <d2d1_3.h>
+#include <wrl.h>
+
+#include <iostream>
+
+namespace element
+{
+
+class d2dFactory
+{
+public:
+    static Microsoft::WRL::ComPtr<ID2D1Factory8>& Get()
+    {
+        static Microsoft::WRL::ComPtr<ID2D1Factory8> factory = nullptr;
+        if (factory == nullptr)
+        {
+            HRESULT hr = D2D1CreateFactory(
+                D2D1_FACTORY_TYPE_MULTI_THREADED,
+                __uuidof(ID2D1Factory8),
+                &factory
+            );
+            if (FAILED(hr))
+            {
+                std::cout << "Failed to create Direct2D factory" << std::endl;
+                exit(1);
+            }
+        }
+
+        return factory;
+    }
+};
+
+}  // namespace element
+
+#endif  // D2DFACTORY_H
