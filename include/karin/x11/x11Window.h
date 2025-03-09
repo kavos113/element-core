d069371be47ec14507911456c9c6c14fb9c7f21f
kavos113 <kohei.hadano@gmail.com>
2025-03-09 11:10:15 +0900

    Add X11 support by creating x11Window class and updating CMakeLists.txt

---
+++ include/karin/x11/x11Window.h
@@ -0,0 +1,15 @@
+#ifndef KARIN_X11_X11WINDOW_H
+#define KARIN_X11_X11WINDOW_H
+
+#include <X11/Xlib.h>
+
+namespace karin
+{
+
+class x11Window
+{
+};
+
+}  // namespace karin
+
+#endif  // KARIN_X11_X11WINDOW_H
---
+++ src/karin/x11/x11Window.cpp
@@ -0,0 +1,5 @@
+#include "x11/x11Window.h"
+
+namespace karin
+{
+}  // namespace karin
